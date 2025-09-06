a17fd86cc21a3c74a28b41f86d07c91931048bb5
Ed Shaziman <erdo.shazy123@gmail.com>
2025-09-06 10:02:09 +0200

    Deploy fix (#4)
    * deploy-fix
    
    * use heroku host
    
    ---------
    
    Co-authored-by: Ed <ed.shaziman@cymantix.com>

--- lani-deploy.sh
+++ lani-deploy.sh
@@ -3,10 +3,5 @@
 # Make sure you have Netlify CLI installed: yarn global add netlify-cli
 # And have run `netlify login` and `netlify init` at least once
 
-set -e
-
-# Build the project
-yarn build
-
-# Deploy to Netlify (change --prod to --draft for preview deploy)
-netlify deploy --draft --dir=build
+yarn workspace @wealth-wing/lani build
+netlify deploy --prod --dir=packages/lani/build
