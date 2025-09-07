bf212857d33f8148ba439dac593cb67e871bc902
Ed Shaziman <erdo.shazy123@gmail.com>
2025-09-07 19:15:05 +0200

    fix the router issue (#6)
    Co-authored-by: Ed <ed.shaziman@cymantix.com>

--- lani-deploy.sh
+++ lani-deploy.sh
@@ -3,5 +3,5 @@
 # Make sure you have Netlify CLI installed: yarn global add netlify-cli
 # And have run `netlify login` and `netlify init` at least once
 
-yarn workspace @wealth-wing/lani build
+yarn workspace @wealth-wing/lani build:prod
 netlify deploy --prod --dir=packages/lani/build
